b3e8bfab480c42d17e6a36979d3c87ab2b0b267b
Mahidhar Reddy <mahidhar@outlook.com>
2021-03-08 19:53:49 +0530

    refactor:redis

--- examples/default/README.md
+++ examples/default/README.md
@@ -12,15 +12,19 @@
 
 | Name | Version |
 |------|---------|
+| terraform | >= 0.13 |
+| aws | >= 3.0 |
 | kubernetes | 1.10.0 |
 | random | ~> 2 |
+| random | >= 3.0 |
+| tls | >= 3.0 |
 
 ## Providers
 
 | Name | Version |
 |------|---------|
 | kubernetes | 1.10.0 |
-| random | ~> 2 |
+| random | ~> 2 >= 3.0 |
 
 ## Inputs
 
---
+++ examples/default/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+  }
+}
