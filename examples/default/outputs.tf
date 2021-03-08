2a1f1b12a1285c8ec7288be8eac0f03341557eac
Mahidhar Reddy <mahidhar@outlook.com>
2021-03-09 00:33:40 +0530

    refactor

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,27 +1,19 @@
 #####
 # Statefulset
 #####
-
 output "statefulset" {
-  value     = module.this.statefulset
-  sensitive = true
+  value = module.this.statefulset
 }
-
 #####
 # Service
 #####
-
 output "service" {
-  value     = module.this.service
-  sensitive = true
+  value = module.this.service
 }
-
 output "secret" {
   value     = module.this.secret
   sensitive = true
 }
-
 output "service_account" {
-  value     = module.this.service_account
-  sensitive = true
+  value = module.this.service_account
 }
